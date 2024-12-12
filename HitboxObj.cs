9902922b69422729006837b73b79442126c4c102
ponbenq <wichitpon045@gmail.com>
2024-12-12 14:23:06 +0700

    add ground

--- HitboxObj.cs
+++ HitboxObj.cs
@@ -10,7 +10,7 @@ public class HitboxObj : RectangleActor
     private int groupCode;
     private RectF rect;
     private float spanTime;
-    public HitboxObj(Vector2 position, RectF rect, int groupCode, float spanTime): base(Color.LightBlue, rect)// new RectF(30, 15, 15, 5))
+    public HitboxObj(Vector2 position, RectF rect, int groupCode, float spanTime): base(Color.Transparent, rect)// new RectF(30, 15, 15, 5))
     {
         this.position = position;
         this.rect = rect;

adbc8334e1ed2bb68824eecc47643ee6e30e815f
tdysq03 <stybm70@gmail.com>
2024-12-19 16:56:42 +0700

    Revert "pull Apicha"
    This reverts commit d18c680bc15c58fd3d36aea4730ef01c2b1190d0, reversing
    changes made to 22958663f3095600238eab9144cc60835c9b2de6.

--- GameTest01.cs
+++ GameTest01.cs
@@ -2,16 +2,11 @@ using ThanaNita.MonoGameTnt;
 using System;
 using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace GameProject;
 
 public class GameTest01 : Game2D
 {
-    private SoundEffect bgmsong;
-    private SoundEffectInstance bgmInstance;
     public GameTest01() : base()
     {
         BackgroundColor = Color.DarkGray;
@@ -20,18 +15,10 @@ public class GameTest01 : Game2D
     protected override void Initialize()
     {
         base.Initialize();
-    }
 
+    }
     protected override void LoadContent()
     {
-        //BGM Music
-
-        bgmsong = SoundEffect.FromFile("bgm.wav");
-        bgmInstance = bgmsong.CreateInstance();
-        bgmInstance.IsLooped = true;
-        bgmInstance.Volume = 0.0f;
-        bgmInstance.Play();
-
         CollisionDetectionUnit.AddDetector(1, 2);
         CollisionDetectionUnit.AddDetector(1,3);
         CollisionDetectionUnit.AddDetector(2,3);
@@ -42,12 +29,5 @@ public class GameTest01 : Game2D
         //All.Add(new PlayerHolder1(ScreenSize));
         //All.Add(new Player(ScreenSize));
         //All.Add(new Player2(ScreenSize));
-        //CollisionDetectionUnit.AddDetector(1, 3);
-        //CollisionDetectionUnit.AddDetector(2, 3);
-        //All.Add(new Floor(new RectF(0, ScreenSize.Y - 150, 1000, 30)));
-        //var player2 = new Player2(ScreenSize);
-        //var player1 = new Player(ScreenSize);
-        //All.Add(player1);
-        //All.Add(player2);
     }
 }

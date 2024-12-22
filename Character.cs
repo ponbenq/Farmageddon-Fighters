0dbffffd9d17c144c1f042a8b79622a13f570d78
tdysq03 <stybm70@gmail.com>
2024-12-22 11:27:44 +0700

    fix ui menu and select screen

--- Character.cs
+++ Character.cs
@@ -9,10 +9,12 @@ namespace GameProject
     public class Character
     {
         private static List<string> spritePaths = new List<string> 
-        { "Resources/sprite/broccoli/broccoli.png",
-            "Resources/sprite/eggplant/eggplant.png",
+        {
             "Resources/sprite/mushroom/mushroom.png",
-            "Resources/sprite/pumpkin/pumpkin.png"
+            "Resources/sprite/broccoli/broccoli.png",
+            "Resources/sprite/pumpkin/pumpkin.png",
+            "Resources/sprite/eggplant/eggplant.png",
+            "Resources/sprite/garlic/garlic.png"
         };
 
         public static string GetSpritePath(int charIndex)
---
+++ MovingBackground.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using ThanaNita.MonoGameTnt;
+namespace GameProject
+{
+    internal class MovingBackground : SpriteActor
+    {
+        private float scrollSpeed;
+        private Texture2D texture;
+        private Vector2 startPosition;
+        private Vector2 screenSize;
+        public MovingBackground(string file, Vector2 screenSize, Vector2 startPosition, float scrollSpeed)
+        {
+            this.scrollSpeed = scrollSpeed;
+            this.screenSize = screenSize;
+            this.startPosition = startPosition;
+            texture = TextureCache.Get(file);
+            float scaleX = screenSize.X / texture.Width;
+            float scaleY = screenSize.Y / texture.Height;
+            float scale = Math.Max(scaleX, scaleY);
+            Position = startPosition;
+            SetTextureRegion(new TextureRegion(texture, new RectF(0, 0, texture.Width, texture.Height)));
+            Scale = new Vector2(scale, scale);
+        }
+        public override void Act(float deltaTime)
+        {
+            var velocity = new Vector2(scrollSpeed, 0);
+            Position += velocity * deltaTime;
+            if (Position.X >= GetTextureWidth())
+            {
+                Position = new Vector2(Position.X - GetTextureWidth() * 2, Position.Y);
+            }
+            base.Act(deltaTime);
+        }
+        public float GetTextureWidth()
+        {
+            return texture.Width * Scale.X;
+        }
+    }
+}
